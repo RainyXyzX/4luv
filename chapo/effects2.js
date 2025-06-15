10b66097711020f2e92f78046c4b6017fa82a43c
RainyXyzX <mharleeguzman@gmail.com>
2025-06-15 13:55:24 +0800

    Delete chapo directory

--- chapo/effects2.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/1056937588394557463?bg=00000000&animated=true=true&hideSpotify=true&showDisplayName=true&hideActivity=true";
-    const imgElement = document.getElementById('rpc');
-    imgElement.src = imgSrc;
-});
