c42683806bcb01a4a54bbbc7e77b678b004ca744
RainyXyzX <mharleeguzman@gmail.com>
2025-06-15 16:32:44 +0800

    Add files via upload

---
+++ cha/effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1270805351041597475?bg=00000000&animated=true=true&hideSpotify=true&showDisplayName=true&hideActivity=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
